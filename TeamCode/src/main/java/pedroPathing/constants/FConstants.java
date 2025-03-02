f19c745e25fe2a57267318e2ecb62edfd0ba42ca
HappyHawks <happyhawkgithub@gmail.com>
2025-03-02 12:34:40 -0600

    Update FConstants.java

--- TeamCode/src/main/java/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -25,8 +25,8 @@ public class FConstants {
         FollowerConstants.xMovement = 57.8741;
         FollowerConstants.yMovement = 52.295;
 
-        FollowerConstants.forwardZeroPowerAcceleration = -41.278;
-        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;
+        FollowerConstants.forwardZeroPowerAcceleration = 41.278;
+        FollowerConstants.lateralZeroPowerAcceleration = 59.7819;
 
         FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0);
         FollowerConstants.useSecondaryTranslationalPID = false;
