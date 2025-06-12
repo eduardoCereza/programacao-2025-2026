d9bc7889d24c9f862e0ed0a70a6d9758258e108b
Twinfone <1468639@sd61learn.ca>
2025-06-11 20:03:51 -0700

    make sum programs. yea

---
+++ TeamCode/src/main/java/Subsystems/Intake.java
@@ -0,0 +1,4 @@
+package Subsystems;
+
+public class Intake {
+}
--- TeamCode/src/main/java/Testing/SensorGoBildaPinpointExample.java
+++ TeamCode/src/main/java/Testing/SensorGoBildaPinpointExample.java
@@ -1,4 +1,4 @@
-package TeleOp;
+package Testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
