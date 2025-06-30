a2fd0bf62281852997fd69ebcc7a8d882c104125
KaizDoodle <13iak13@gmail.com>
2025-06-29 19:21:33 -0700

    create limelight subsystem with get april tag method

--- TeamCode/src/main/java/Subsystems/LimelightSubsystem.java
+++ TeamCode/src/main/java/Subsystems/LimelightSubsystem.java
@@ -1,8 +1,37 @@
 package Subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+import java.util.List;
 
 public class LimelightSubsystem extends SubsystemBase {
     public Limelight3A limelight;
+
+    public LimelightSubsystem(HardwareMap hardwareMap){
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+    }
+
+    //returns list of april tags seen at a given time
+    public List<LLResultTypes.FiducialResult> getAprilTag(){
+        //Get detection info
+       return limelight.getLatestResult().getFiducialResults();
+    }
+
+    public void getDetections(){
+
+    }
+
+    public void getDetections(double numDetections){
+        //Get detection info
+        LLResult result = limelight.getLatestResult();
+
+        //array list of result time
+
+
+    }
 }
