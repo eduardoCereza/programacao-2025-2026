67e86a26db5e8023cb34b59f270de0ee59ca42f3
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-08-20 17:47:05 -0300

    criandos métodos obrigatório

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Modo_Teleoperado.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Modo_Teleoperado.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
-public class Modo_Teleoperado {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class Modo_Teleoperado extends OpMode {
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void loop(){
+        
+    }
 }
