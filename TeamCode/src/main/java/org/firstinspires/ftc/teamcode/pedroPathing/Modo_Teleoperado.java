aff7c0abf4ff52b8364d360f579b2a280e3a17ac
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-08-20 17:54:30 -0300

    Começando a criar algumas das variáveis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Modo_Teleoperado.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Modo_Teleoperado.java
@@ -1,16 +1,23 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Modo_Teleoperado extends OpMode {
 
+    //Criando variáveis do motor do atuador
+    DcMotor left, right, slide;
+
+    //Criando variáveis de condição do atuador
+    int limitSlide;
+    
     @Override
     public void init(){
-
+        
     }
 
     @Override
     public void loop(){
-        
+
     }
 }
